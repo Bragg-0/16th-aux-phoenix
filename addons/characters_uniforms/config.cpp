01db77228f5ddb58d0046576cd96cb648c78669d
Bragg <maxime.brochet.98@gmail.com>
2025-08-06 19:59:20 +0200

    Ajout des fichiers de configuration pour les uniformes et les armes des personnages Customs et Regs, ainsi que des textures associées.

--- addons/characters_uniforms/config.cpp
+++ addons/characters_uniforms/config.cpp
@@ -8,9 +8,51 @@ class CfgPatches {
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = { QUOTE(ADDON) };
-        units[] = {};
-        weapons[] = {};
+        units[] = {
+            // Regs
+                #define CHARACTERS_TYPE Regs
+                QCVARF(Shiny),
+                QECVARF(Shiny,EOD),
+                QECVARF(Shiny,Medic),
+                QCVARF(Trooper),
+                QECVARF(Trooper,EOD),
+                QECVARF(Trooper,Medic),
+                #undef CHARACTERS_TYPE
+
+            // Customs
+                #define CHARACTERS_TYPE Customs
+                //* Add the class names for custom vests here
+                #undef CHARACTERS_TYPE
+        };
+        weapons[] = {
+            // Regs
+                #define CHARACTERS_TYPE Regs
+                QCVAR(Shiny),
+                QECVAR(Shiny,EOD),
+                QECVAR(Shiny,Medic),
+                QCVAR(Trooper),
+                QECVAR(Trooper,EOD),
+                QECVAR(Trooper,Medic),
+                #undef CHARACTERS_TYPE
+
+            // Customs
+                #define CHARACTERS_TYPE Customs
+                //* Add the class names for custom vests here
+                #undef CHARACTERS_TYPE
+
+        };
         VERSION_CONFIG;
     };
 };
 
+class CfgVehicles {
+    class lsd_gar_phase2_base;
+    #include "CfgVehicles_Regs.hpp"
+
+};
+
+class CfgWeapons {
+    CLASS_INHERITANCE(ls_gar_phase2_uniform);
+    #include "CfgWeapons_Regs.hpp"
+
+};
