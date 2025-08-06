d973deacdb18c3e6e2a5bd0e3c2e2e58962ebe7d
Bragg <maxime.brochet.98@gmail.com>
2025-08-06 21:35:32 +0200

    Ajout des fichiers de configuration pour les personnages Customs et Regs, incluant les définitions des sacs à dos et des vests personnalisés.

--- addons/characters_backpacks/config.cpp
+++ addons/characters_backpacks/config.cpp
@@ -8,9 +8,32 @@ class CfgPatches {
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = { QUOTE(ADDON) };
-        units[] = {};
+        units[] = {
+            // Regs
+                #define CHARACTERS_TYPE Regs
+                QECVAR(Base,Medic),
+                QECVAR(Base,Standard),
+                QECVAR(EOD,Standard),
+                QECVAR(Base,Radio),
+                QECVAR(Base,Rocket),
+                QECVAR(Base,Invisible),
+                #undef CHARACTERS_TYPE
+
+            // Customs
+                #define CHARACTERS_TYPE Customs
+                //* Add the class names for custom vests here
+                #undef CHARACTERS_TYPE
+        };
         weapons[] = {};
         VERSION_CONFIG;
     };
 };
 
+class CfgVehicles {
+    class ls_gar_heavyMedic_backpack;
+    class ls_gar_heavy_backpack;
+    class ls_gar_mediumRadio_backpack;
+    class ls_gar_rocket_backpack;
+    #include "CfgVehicles_Regs.hpp"
+    #include "CfgVehicles_Customs.hpp"
+};
