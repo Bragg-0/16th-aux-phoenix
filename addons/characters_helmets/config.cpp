99785e97f7b4fc0e1f2228e41d8af920284a466b
Bragg <maxime.brochet.98@gmail.com>
2025-08-06 20:15:33 +0200

    Ajout des fichiers de configuration pour les casques et les armes des personnages Customs et Regs, ainsi que l'inclusion des définitions dans le fichier config.cpp.

--- addons/characters_helmets/config.cpp
+++ addons/characters_helmets/config.cpp
@@ -8,9 +8,47 @@ class CfgPatches {
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = { QUOTE(ADDON) };
-        units[] = {};
-        weapons[] = {};
+        units[] = {
+            // Regs
+                #define CHARACTERS_TYPE Regs
+                QECVARF(Shiny,P2),
+                QECVARF(Shiny,P2_Pilot),
+                QECVARF(Trooper,P2),
+                QECVARF(Trooper,P2_Pilot),
+                #undef CHARACTERS_TYPE
+
+            // Customs
+                #define CHARACTERS_TYPE Customs
+                //* Add the class names for custom vests here
+                #undef CHARACTERS_TYPE
+        };
+        weapons[] = {
+            // Regs
+                #define CHARACTERS_TYPE Regs
+                QECVAR(Shiny,P2),
+                QECVAR(Shiny,P2_Pilot),
+                QECVAR(Trooper,P2),
+                QECVAR(Trooper,P2_Pilot),
+                #undef CHARACTERS_TYPE
+
+            // Customs
+                #define CHARACTERS_TYPE Customs
+                //* Add the class names for custom vests here
+                #undef CHARACTERS_TYPE
+        };
         VERSION_CONFIG;
     };
 };
 
+class CfgVehicles {
+    class Headgear_Base_F;
+    #include "CfgVehicles_Regs.hpp"
+    #include "CfgVehicles_Customs.hpp"
+};
+
+class CfgWeapons {
+    CLASS_INHERITANCE(ls_gar_phase2_helmet);
+    CLASS_INHERITANCE(ls_gar_phase2_pilot_helmet);
+    #include "CfgWeapons_Regs.hpp"
+    #include "CfgWeapons_Customs.hpp"
+};
