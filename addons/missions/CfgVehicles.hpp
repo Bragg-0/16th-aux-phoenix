b8bef1ab1a3fa63de1d1df4454fe585be957dd79
Bragg <maxime.brochet.98@gmail.com>
2025-08-13 17:31:40 +0200

    Correction de l'utilisation des tableaux dans les conditions et les déclarations des actions d'auto-protection auditive

--- addons/missions/CfgVehicles.hpp
+++ addons/missions/CfgVehicles.hpp
@@ -5,14 +5,14 @@ class CfgVehicles {
         class ACE_SelfActions {
             class GVAR(activateEarProtection) {
                 displayName = CSTRING(activateEarProtection);
-                condition = QUOTE([_player,1] call FUNC(canEarProtection));
-                statement = QUOTE([_player,true] call FUNC(updateEarProtection));
+                condition = QUOTE([ARR_2(_player,1)] call FUNC(canEarProtection));
+                statement = QUOTE([ARR_2(_player,true)] call FUNC(updateEarProtection));
                 icon = QPATHTOF(data\waves_ca.paa);
             };
             class GVAR(deactivateEarProtection) {
                 displayName = CSTRING(deactivateEarProtection);
-                condition = QUOTE([_player,0] call FUNC(canEarProtection));
-                statement = QUOTE([_player,false] call FUNC(updateEarProtection));
+                condition = QUOTE([ARR_2(_player,0)] call FUNC(canEarProtection));
+                statement = QUOTE([ARR_2(_player,false)] call FUNC(updateEarProtection));
                 icon = QPATHTOF(data\unwaves_ca.paa);
             };
         };
