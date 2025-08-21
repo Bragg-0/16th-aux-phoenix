be9a71c1a44c196579aa926064873c212ad56ff5
Bragg <maxime.brochet.98@gmail.com>
2025-08-21 19:05:08 +0200

    Renommage de la classe ADDON en SUBADDON dans CfgEventHandlers.hpp pour une meilleure clarté.

--- addons/dialogs_loadout/CfgEventHandlers.hpp
+++ addons/dialogs_loadout/CfgEventHandlers.hpp
@@ -1,11 +1,11 @@
 class Extended_PreStart_EventHandlers {
-    class ADDON {
+    class SUBADDON {
         init = QUOTE(call COMPILE_SCRIPT(XEH_preStart));
     };
 };
 
 class Extended_PreInit_EventHandlers {
-    class ADDON {
+    class SUBADDON {
         init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
     };
 };
