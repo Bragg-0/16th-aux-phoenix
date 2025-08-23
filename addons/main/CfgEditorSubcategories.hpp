c793cebfd0653523514d2223ad1f8819dbeb0437
Bragg <maxime.brochet.98@gmail.com>
2025-08-23 21:25:31 +0200

    Ajout de la catégorie de véhicules "Laat" dans CfgEditorSubcategories.hpp avec le nom d'affichage approprié.

--- addons/main/CfgEditorSubcategories.hpp
+++ addons/main/CfgEditorSubcategories.hpp
@@ -8,4 +8,7 @@ class CfgEditorSubcategories {
     class GVAR(Characters_Regs_NCO_Officer) {
         displayName = CSTRING(Characters_Regs_NCO_Officer);
     };
+    class GVAR(Vehicles_Laat) {
+        displayName = CSTRING(Vehicles_Laat);
+    };
 };
