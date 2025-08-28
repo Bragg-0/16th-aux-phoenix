0eb323b763c39ad203bd40e9673ec1da9b42b909
Bragg <maxime.brochet.98@gmail.com>
2025-08-28 06:38:57 +0200

    Ajout de nouvelles textures pour les véhicules LAAT et mise à jour des définitions de texture dans CfgVehicles.hpp et script_macros.hpp. Modification des chaînes de caractères dans stringtable.xml pour refléter les nouvelles textures.

--- addons/vehicles_laat/CfgVehicles.hpp
+++ addons/vehicles_laat/CfgVehicles.hpp
@@ -1,6 +1,6 @@
 class CfgVehicles {
 
-    //LAAT_I
+    // LAAT_I
     class 3as_LAAT_Mk1;
     class 3as_LAAT_Mk1Lights;
     class 3as_LAAT_Mk2;
@@ -9,107 +9,55 @@ class CfgVehicles {
     class ESUBGVAR(I,Mk1) : 3as_LAAT_Mk1 {
         BASE_LAAT;
         displayName = SUBCSTRING(I_Mk1);
-        hiddenSelections[] = { "camo" };
-        hiddenSelectionsTextures[] = {
-            QPATHTOF(data\LAAT_I\Hull1_co.paa)
-        };
-        class textureSources {
-            class SUBGVAR(texture_0) {
-                displayName = SUBCSTRING(I_texture_0);
-                author = AUTHOR;
-                textures[] = {
-                    QPATHTOF(data\LAAT_I\Hull1_co.paa)
-                };
-            };
-        };
-        textureList[] = {
-            QSUBGVAR(texture_0), 1
-        };
+        TEXTURES_LAAT_I;
     };
 
     class ESUBGVAR(I,Mk1Lights) : 3as_LAAT_Mk1Lights {
         BASE_LAAT;
         displayName = SUBCSTRING(I_Mk1Lights);
-        hiddenSelections[] = { "camo" };
-        hiddenSelectionsTextures[] = {
-            QPATHTOF(data\LAAT_I\Hull1_co.paa)
-        };
-        class textureSources {
-            class SUBGVAR(texture_0) {
-                displayName = SUBCSTRING(I_texture_0);
-                author = AUTHOR;
-                textures[] = {
-                    QPATHTOF(data\LAAT_I\Hull1_co.paa)
-                };
-            };
-        };
-        textureList[] = {
-            QSUBGVAR(texture_0), 1
-        };
+        TEXTURES_LAAT_I;
     };
 
     class ESUBGVAR(I,Mk2) : 3as_LAAT_Mk2 {
         BASE_LAAT;
         displayName = SUBCSTRING(I_Mk2);
-        hiddenSelections[] = { "camo" };
-        hiddenSelectionsTextures[] = {
-            QPATHTOF(data\LAAT_I\Hull1_co.paa)
-        };
-        class textureSources {
-            class SUBGVAR(texture_0) {
-                displayName = SUBCSTRING(I_texture_0);
-                author = AUTHOR;
-                textures[] = {
-                    QPATHTOF(data\LAAT_I\Hull1_co.paa)
-                };
-            };
-        };
-        textureList[] = {
-            QSUBGVAR(texture_0), 1
-        };
+        TEXTURES_LAAT_I;
     };
 
     class ESUBGVAR(I,Mk2Lights) : 3as_LAAT_Mk2Lights {
         BASE_LAAT;
         displayName = SUBCSTRING(I_Mk2Lights);
-        hiddenSelections[] = { "camo" };
-        hiddenSelectionsTextures[] = {
-            QPATHTOF(data\LAAT_I\Hull1_co.paa)
-        };
-        class textureSources {
-            class SUBGVAR(texture_0) {
-                displayName = SUBCSTRING(I_texture_0);
-                author = AUTHOR;
-                textures[] = {
-                    QPATHTOF(data\LAAT_I\Hull1_co.paa)
-                };
-            };
-        };
-        textureList[] = {
-            QSUBGVAR(texture_0), 1
-        };
+        TEXTURES_LAAT_I;
     };
 
-    //LAAT_C
+    // LAAT_C
     class 3AS_LAATC;
     class SUBGVAR(C) : 3AS_LAATC {
         BASE_LAAT;
         displayName = SUBCSTRING(C);
         hiddenSelections[] = { "camo" };
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\LAAT_C\Hull1_co.paa)
+            QPATHTOF(data\LAAT_C\Hull_var0_co.paa)
         };
         class textureSources {
             class SUBGVAR(texture_0) {
                 displayName = SUBCSTRING(C_texture_0);
                 author = AUTHOR;
                 textures[] = {
-                    QPATHTOF(data\LAAT_C\Hull1_co.paa)
+                    QPATHTOF(data\LAAT_C\Hull_var0_co.paa)
+                };
+            };
+            class SUBGVAR(texture_1) {
+                displayName = SUBCSTRING(C_texture_1);
+                author = AUTHOR;
+                textures[] = {
+                    QPATHTOF(data\LAAT_C\Hull_var1_co.paa)
                 };
             };
         };
         textureList[] = {
-            QSUBGVAR(texture_0), 1
+            QSUBGVAR(texture_0), 1,
+            QSUBGVAR(texture_1), 1
         };
     };
 };
--- addons/vehicles_laat/script_macros.hpp
+++ addons/vehicles_laat/script_macros.hpp
@@ -12,3 +12,37 @@
     EMPTY_ITEM;                                      \
     EMPTY_WEAPON;                                    \
     EMPTY_MAGAZINE
+
+#define TEXTURES_LAAT_I                                \
+    hiddenSelections[] = { "camo" };                   \
+    hiddenSelectionsTextures[] = {                     \
+        QPATHTOF(data\LAAT_I\Hull_var0_co.paa)         \
+    };                                                 \
+    class textureSources {                             \
+        class SUBGVAR(texture_0) {                     \
+            displayName = SUBCSTRING(I_texture_0);     \
+            author = AUTHOR;                           \
+            textures[] = {                             \
+                QPATHTOF(data\LAAT_I\Hull_var0_co.paa) \
+            };                                         \
+        };                                             \
+        class SUBGVAR(texture_1) {                     \
+            displayName = SUBCSTRING(I_texture_1);     \
+            author = AUTHOR;                           \
+            textures[] = {                             \
+                QPATHTOF(data\LAAT_I\Hull_var1_co.paa) \
+            };                                         \
+        };                                             \
+        class SUBGVAR(texture_2) {                     \
+            displayName = SUBCSTRING(I_texture_2);     \
+            author = AUTHOR;                           \
+            textures[] = {                             \
+                QPATHTOF(data\LAAT_I\Hull_var2_co.paa) \
+            };                                         \
+        };                                             \
+    };                                                 \
+    textureList[] = {                                  \
+        QSUBGVAR(texture_0), 1,                        \
+        QSUBGVAR(texture_1), 1,                        \
+        QSUBGVAR(texture_2), 1                         \
+    }
