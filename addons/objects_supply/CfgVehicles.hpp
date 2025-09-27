a75b50d40d256503d870ffa3edfe828c2f3e6c77
Bragg-0 <maxime.brochet.98@gmail.com>
2025-09-27 12:05:35 +0200

    Ajout de la configuration des boîtes d'armement, de munitions, de grenades et médicales avec des armes et des articles spécifiques.

--- addons/objects_supply/CfgVehicles.hpp
+++ addons/objects_supply/CfgVehicles.hpp
@@ -10,9 +10,15 @@ class CfgVehicles {
         BASE_SUPPLY;
         EMPTY_ITEM;
         EMPTY_BACKPACK;
-        EMPTY_WEAPON;
+        class TransportWeapons {
+            WEAP_XX(3AS_DC15S_F,5);
+            WEAP_XX(3AS_DC15A_F,5);
+            WEAP_XX(3AS_DC15A_GL,2);
+            WEAP_XX(3AS_DC17S_F,10);
+            WEAP_XX(JLTS_Z6,2);
+            WEAP_XX(3AS_DC15X_F,2);
+        };
         EMPTY_MAGAZINE;
-        //TODO: Configure weapons box
     };
 
     class SUBGVAR(ammo) : JLTS_Ammobox_ammo_GAR {
@@ -21,8 +27,13 @@ class CfgVehicles {
         EMPTY_ITEM;
         EMPTY_BACKPACK;
         EMPTY_WEAPON;
-        EMPTY_MAGAZINE;
-        //TODO: Configure ammo box
+        class TransportMagazines {
+            MAG_XX(3AS_16Rnd_EC20_Mag,50);
+            MAG_XX(3AS_10Rnd_EC100_Mag,30);
+            MAG_XX(3AS_45Rnd_EC50_Mag,20);
+            MAG_XX(3AS_60Rnd_EC30_mag,20);
+            MAG_XX(JLTS_Z6_mag,10);
+        };
     };
 
     class SUBGVAR(grenades) : JLTS_Ammobox_grenades_GAR {
@@ -31,8 +42,20 @@ class CfgVehicles {
         EMPTY_ITEM;
         EMPTY_BACKPACK;
         EMPTY_WEAPON;
-        EMPTY_MAGAZINE;
-        //TODO: Configure grenades box
+        class TransportMagazines {
+            MAG_XX(3AS_3UGL_MK55_Smoke_Red_shell,10);
+            MAG_XX(3AS_3UGL_MK55_Smoke_Green_shell,10);
+            MAG_XX(3AS_3UGL_MK55_Smoke_Blue_shell,10);
+            MAG_XX(3AS_3UGL_MK56_Flare_White_shell,10);
+            MAG_XX(3AS_3UGL_MK56_Flare_Red_shell,10);
+            MAG_XX(3AS_3UGL_MK56_Flare_Green_shell,10);
+            MAG_XX(3AS_SmokeWhite,10);
+            MAG_XX(3AS_SmokeRed,10);
+            MAG_XX(3AS_SmokeGreen,10);
+            MAG_XX(3AS_SmokeBlue,10);
+            MAG_XX(3AS_ThermalDetonator,10);
+            MAG_XX(3AS_BaridumCore,5);
+        };
     };
 
     class SUBGVAR(launchers) : JLTS_Ammobox_launchers_GAR {
@@ -40,18 +63,38 @@ class CfgVehicles {
         BASE_SUPPLY;
         EMPTY_ITEM;
         EMPTY_BACKPACK;
-        EMPTY_WEAPON;
-        EMPTY_MAGAZINE;
-        //TODO: Configure launchers box
+        class TransportWeapons {
+            WEAP_XX(3AS_RPS6_HP,5);
+        };
+        class TransportMagazines {
+            MAG_XX(3AS_MK41_AT,20);
+            MAG_XX(3AS_MK42_HE,10);
+        };
     };
 
     class SUBGVAR(medical) : JLTS_Ammobox_support_GAR {
         displayName = CSTRING(Medical);
         BASE_SUPPLY;
-        EMPTY_ITEM;
+        class TransportItems {
+            ITEM_XX(PC_Medical_ElasticBandage,100);
+            ITEM_XX(PC_Medical_PackingBandage,100);
+            ITEM_XX(PC_Medical_Morphine,50);
+            ITEM_XX(PC_Medical_Adenosine,20);
+            ITEM_XX(PC_Medical_SurgicalKit,10);
+            ITEM_XX(PC_Medical_Epinephrine,50);
+            ITEM_XX(PC_Medical_PersonalAidKit,10);
+            ITEM_XX(PC_Medical_Splint,50);
+            ITEM_XX(ACE_tourniquet,50);
+            ITEM_XX(PC_Medical_SalineIV_250,50);
+            ITEM_XX(PC_Medical_SalineIV_500,30);
+            ITEM_XX(PC_Medical_PlasmaIV_500,30);
+            ITEM_XX(PC_Medical_BloodIV,20);
+            ITEM_XX(PC_Medical_Quikclot,100);
+        };
         EMPTY_BACKPACK;
         EMPTY_WEAPON;
-        EMPTY_MAGAZINE;
-        //TODO: Configure medical box
+        class TransportMagazines {
+            MAG_XX(PC_Medical_Painkillers,20);
+        };
     };
 };
