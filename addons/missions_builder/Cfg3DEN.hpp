046574097c25e91850fe729e64df3d96089d548f
Bragg-0 <maxime.brochet.98@gmail.com>
2025-10-03 20:08:08 +0200

    Add base composition and localization strings for missions builder
    - Created a new header file for the "[16th] Base" composition, including required addons.
    - Added localization entries for the "[16th] Base" in the stringtable.xml for English and French.

--- addons/missions_builder/Cfg3DEN.hpp
+++ addons/missions_builder/Cfg3DEN.hpp
@@ -1,15 +1,19 @@
 class Cfg3DEN {
     class Compositions {
-        class GVAR(Venator) {
-            path = "x\pc\addons\missions_builder\data\compositions\Venator"; //! DON'T MAKE A `\` BEFORE `x\pc\addons\*`
+        class GVAR(Base) {
+            path = "x\pc\addons\missions_builder\data\compositions\Base"; //! DON'T MAKE A `\` BEFORE `x\pc\addons\*`
             side = UNKNOWN;
             editorCategory = QEGVAR(main,3den);
             editorSubcategory = "EdSubcat_Utilities";
             scope = 2;
             scopeCurator = 1;
-            displayName = CSTRING(Venator);
+            displayName = CSTRING(Base);
             icon = QPATHTOF(data\compositions\logo_ca.paa);
             useSideColorOnIcon = 1;
+        };
+        class GVAR(Venator) : GVAR(Base) {
+            path = "x\pc\addons\missions_builder\data\compositions\Venator"; //! DON'T MAKE A `\` BEFORE `x\pc\addons\*`
+            displayName = CSTRING(Venator);
         };
     };
 };
