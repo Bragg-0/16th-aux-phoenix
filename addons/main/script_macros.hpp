41deb64c4a6811086b346c64423cf279c1cb7177
Bragg-0 <189566294+Bragg-0@users.noreply.github.com>
2026-05-30 13:35:17 +0200

    Désactivation des définitions de mode de débogage et de cache de compilation

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,9 +1,9 @@
 #define DEBUG_SYNCHRONOUS
 
 //! Comment the following line to disable debug mode (requires for release build)
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
-#define ENABLE_PERFORMANCE_COUNTERS
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
 
 #include "\x\cba\addons\main\script_macros_common.hpp"
 #include "\x\cba\addons\xeh\script_xeh.hpp"
